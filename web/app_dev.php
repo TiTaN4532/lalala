6a0e963ed18a2de32edfbfae9c50ad3310e2054f
PAvel <derevyanko.pav@mail.ru>
2014-03-30 16:54:33 -0400

    asd

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,5 +1,4 @@
 <?php
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
@@ -11,7 +10,7 @@ umask(0000);
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '46.185.110.251','fe80::1', '::1'))
+    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '195.230.103.1', '46.185.110.251','fe80::1', '::1'))
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
